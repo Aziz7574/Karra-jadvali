9ec2b098ba0ff2ede6e8dd406a58a290dbc02d66
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2023-07-27 11:36:19 +0500

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Transactions;
+
+namespace Space
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Choose options");
+            Console.WriteLine("Choose 1 for all times-table");
+            Console.WriteLine("Choose 2 to print times table till n th number");
+
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        Console.WriteLine($"{i} * {j} = {i * j}");
+                    }
+                    Console.WriteLine("----------");
+                }
+            }
+            else if (option == "2")
+            {
+                Console.WriteLine("Enter the number to print the time table untill it");
+                int number = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= number; i++)
+                {
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        Console.WriteLine($"{i} * {j} = {i * j}");
+                    }
+                    Console.WriteLine("----------");
+                }
+            }
+
+            Console.WriteLine("Choose 1 to continue");
+            Console.WriteLine("Click anything to quit");
+
+            option = Console.ReadLine();
+            if (option == "1")
+                Main(args);
+            else return;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Karra-jadvali
+# Multiplication table
+
+This program shows multiplication table 
+
+![Alt text](multiplication-tables.png)
+-
+-
+
+_
+_
+
+[You can find the source code here](https://github.com/Aziz7574/Multiplication-table)
